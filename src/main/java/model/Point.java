0e1837ce9434b3909106dd80a4cdde37450bfeb0
palatin <igorshamin96@gmail.com>
2018-07-21 13:05:36 +0300

    add point model

---
+++ src/main/java/model/Point.java
@@ -0,0 +1,40 @@
+package model;
+
+public class Point {
+
+    public int x,y,z;
+
+    @Override
+    public boolean equals(Object obj) {
+        Point p = (Point) obj;
+        return x == p.x && y == p.y && z == p.z;
+    }
+
+    public Point add(float value) {
+        this.x += value;
+        this.y += value;
+        this.z += value;
+        return this;
+    }
+
+    public Point add(Point point) {
+        this.x += point.x;
+        this.y += point.y;
+        this.z += point.z;
+        return this;
+    }
+
+    public Point divide(float value) {
+        this.x /= value;
+        this.y /= value;
+        this.z /= value;
+        return this;
+    }
+
+    public Point divide(Point point) {
+        this.x /= point.x;
+        this.y /= point.y;
+        this.z /= point.z;
+        return this;
+    }
+}
